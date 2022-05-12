669554391bdaed61d6e643e98f091bb5682df943
Daniel Frutos <danielfrutosgonzalez@gmail.com>
2022-05-12 14:20:19 -0300

    agrego proyecto

---
+++ src/main/java/com/unla/Grupo20OO22022/Grupo20Oo22022Application.java
@@ -0,0 +1,13 @@
+package com.unla.Grupo20OO22022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo20Oo22022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo20Oo22022Application.class, args);
+	}
+
+}
